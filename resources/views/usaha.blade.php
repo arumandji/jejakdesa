5dc3ee1db73d15134cb27c92a5c3db73dd3e79af
abdillahemwe <abdillahemwe@gmail.com>
2024-12-18 16:46:46 +0700

    Jelo commit

--- resources/views/usaha.blade.php
+++ resources/views/usaha.blade.php
@@ -52,13 +52,9 @@
     <div class="page-title dark-background" data-aos="fade" style="background-image: url(assets/img/page-title-bg.webp);">
       <div class="container position-relative">
         <h1>Services</h1>
-        <p>
-          Home
-          /
-          Services</p>
         <nav class="breadcrumbs">
           <ol>
-            <li><a href="index.html">Home</a></li>
+            <li><a href="{{ url('/main') }}">Home</a></li>
             <li class="current">Services</li>
           </ol>
         </nav>

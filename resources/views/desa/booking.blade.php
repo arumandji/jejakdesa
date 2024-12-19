ddb634fe065be0b619c0ec8c2e2d1e7cfb85704e
Yuro Arumandji <arumandji04@gmail.com>
2024-12-19 17:03:42 +0700

    arum commit

--- resources/views/desa/booking.blade.php
+++ resources/views/desa/booking.blade.php
@@ -23,7 +23,7 @@
       </div>
     </div><!-- End Page Title -->
 
-    <!-- Blog Posts 2 Section -->
+    {{-- <!-- Blog Posts 2 Section -->
     <section id="blog-posts-2" class="blog-posts-2 section">
 
       <div class="container">
@@ -205,7 +205,80 @@
         </div>
       </div>
 
-    </section><!-- /Blog Pagination Section -->
+    </section><!-- /Blog Pagination Section --> --}}
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact section">
+
+      <div class="container" data-aos="fade">
+
+        <div class="row gy-5 gx-lg-5">
+
+          <div class="col-lg-4">
+
+            <div class="info">
+              <h3>Pemesanan</h3>
+              <p>Kami hadir untuk memperkenalkan desa, budaya,dan mendukung perekonomian lokal.</p>
+
+              <div class="info-item d-flex">
+                <i class="bi bi-geo-alt flex-shrink-0"></i>
+                <div>
+                  <h4>Location:</h4>
+                  <p>Surabaya, Indonesia</p>
+                </div>
+              </div><!-- End Info Item -->
+
+              <div class="info-item d-flex">
+                <i class="bi bi-envelope flex-shrink-0"></i>
+                <div>
+                  <h4>Email:</h4>
+                  <p>jejakdesa@unair.ac.id</p>
+                </div>
+              </div><!-- End Info Item -->
+
+              <div class="info-item d-flex">
+                <i class="bi bi-phone flex-shrink-0"></i>
+                <div>
+                  <h4>Call:</h4>
+                  <p>+62 8315 0559 9789</p>
+                </div>
+              </div><!-- End Info Item -->
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-8">
+            <form action="forms/booking.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" placeholder="Message" required=""></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div><!-- End Contact Form -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Contact Section -->
+
   </main>
 
   @include('layouts.footer')
